a4d1b61d14f198cd1a1380ac493aea7c77383637
eulerworks <eulerworks@gmail.com>
2015-02-25 22:53:37 -0800

    GPIO tests

--- CS_TEST_EVAL/Src/main.c
+++ CS_TEST_EVAL/Src/main.c
@@ -113,12 +113,12 @@ int main(void)
   {
 	uint32_t index;
 	//if(1==HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_10)){
-		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_12, GPIO_PIN_SET);
   	//}
 	//else HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11, GPIO_PIN_RESET);	
 		for(index; index<400000; index++)
 		asm("nop");
-		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11, GPIO_PIN_RESET);	
+		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_12, GPIO_PIN_RESET);	
 		for(index; index<400000; index++)
 		asm("nop");
   }
